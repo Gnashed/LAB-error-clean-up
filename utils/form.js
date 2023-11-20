623a05cb95eb5bf65ee1c899764066514cc021e6
Tion Blackmon <tionblackmon@icloud.com>
2023-11-19 21:36:54 -0600

    Took it a step further and moved js files into components and utils.

---
+++ utils/form.js
@@ -0,0 +1,52 @@
+// Importing utils
+import renderToDOM from './renderToDOM';
+import studentsOnDom from './studentsOnDom';
+import createId from './createId';
+
+// Importing data
+import { students, houses } from './sample_data/data';
+
+// sorts student to a house and then place them in the students array
+const sortStudent = (e) => {
+  e.preventDefault();
+  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+  if (e.target.id === 'sorting') {
+    const student = document.querySelector('#student-name');
+
+    // create the new student object
+    students.push({
+      id: createId(students),
+      name: student.value,
+      house: sortingHat.house,
+      crest: sortingHat.crest
+    });
+
+    student.value = ''; // reset value of input
+    studentsOnDom('#students', students);
+  }
+};
+
+// add form to DOM on start-sorting click.
+// Add events for form after the form is on the DOM
+const form = () => {
+  const domString = `<form id="sorting" class="d-flex flex-column form-floating ">
+    <input
+    type="text"
+    class="form-control mb-1"
+    id="student-name"
+    placeholder="Enter a name"
+    required
+  />
+  <label for="floatingInputValue">Name to be sorted</label>
+<button type="submit" class="btn btn-success">Get Sorted!</button>
+</form>`;
+
+  renderToDOM('#form-container', domString);
+
+  // has to be put on the DOM after form is on DOM, not before
+  // on form submit, sort student
+  document.querySelector('#sorting').addEventListener('submit', sortStudent);
+};
+
+export default { sortStudent, form };
